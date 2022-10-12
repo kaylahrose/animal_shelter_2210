320a4c33c978a7ca6d84192efd428083adf2e602
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-10-12 08:22:00 -0700

    Add Shelter class attributes

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,2 +1,11 @@
 class Shelter
+    attr_reader :name
+    attr_reader :capacity
+    attr_reader :pets
+
+    def initialize(name, pets)
+        @name = name
+        @capacity = pets
+        @pets = []
+    end
 end
