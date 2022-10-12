434d849a991a71128a4ae3341b4c577e93b047bb
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-10-12 09:05:30 -0700

    Complete iteration 2

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -2,10 +2,25 @@ class Shelter
     attr_reader :name
     attr_reader :capacity
     attr_reader :pets
+    attr_reader :patrons
 
     def initialize(name, pets)
         @name = name
         @capacity = pets
         @pets = []
+        @patrons = @pets
     end
+
+    def add_pet(pet_name)
+        @pets << pet_name
+    end
+
+    def call_pets
+        call_pets = []
+        pets.each do |pet|
+            call_pets << pet += '!'
+        end
+        p call_pets
+    end
+
 end
--- spec/shelter_spec.rb
+++ spec/shelter_spec.rb
@@ -6,28 +6,28 @@ RSpec.describe Shelter do
   # Iteration 1
   describe '#initialize' do
     it 'is a Shelter' do
-      skip
+      
 
       shelter = Shelter.new('Denver Animal Shelter', 5)
       expect(shelter).to be_a(Shelter)
     end
 
     it 'can read the name' do
-      skip
+      
 
       shelter = Shelter.new('Denver Animal Shelter', 5)
       expect(shelter.name).to eq('Denver Animal Shelter')
     end
 
     it 'can read the capacity' do
-      skip
+      
 
       shelter = Shelter.new('Denver Animal Shelter', 5)
       expect(shelter.capacity).to eq(5)
     end
 
     it 'has no pets by default' do
-      skip
+      
 
       shelter = Shelter.new('Denver Animal Shelter', 5)
       expect(shelter.pets).to eq []
@@ -37,7 +37,7 @@ RSpec.describe Shelter do
   # Iteration 2
   describe '#add_pet' do
     it 'returns a list of pets' do
-      skip
+      
 
       shelter = Shelter.new('Denver Animal Shelter', 5)
       shelter.add_pet('Salem')
@@ -51,7 +51,7 @@ RSpec.describe Shelter do
 
   describe '#call_pets' do
       it 'returns a list of names with exclamation points appended' do
-        skip 
+         
 
         shelter = Shelter.new('Denver Animal Shelter', 5)
         shelter.add_pet('Salem')
@@ -63,3 +63,4 @@ RSpec.describe Shelter do
       end
     end
 end
+
